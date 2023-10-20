8e43a868175229abcc097a1cc8b20790c05ddb6d
Hank Magnuski <hank.magnuski@gmail.com>
2023-10-20 13:59:10 -0700

    Added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+# dpkgs needed
+sudo apt-get -y install python3
+sudo apt-get -y install libglfw3
+sudo apt-get -y install make
+sudo apt-get -y install gcc
+
+# Python packages needed
+sudo python3 -m pip install tensorflow
