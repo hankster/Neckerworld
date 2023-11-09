16bffaa1106d261bcbb84d92ac24cc806a6f2f0d
Hank Magnuski <hank.magnuski@gmail.com>
2023-11-08 20:58:00 -0800

    Edited install list.

--- requirements.sh
+++ requirements.sh
@@ -1,16 +1,19 @@
 #! /usr/bin/bash
 
+# update with the latest firmware
+sudo apt-get update
+
 # dpkgs needed
 sudo apt-get -y install python3
-sudo apt-get -y install make
+sudo apt-get -y install make cmake pkg-config
 sudo apt-get -y install gcc
-sudo apt-get -y install opengl
-sudo apt-get -y install libglfw3
-sudo apt-get -y install mesa-utils lib glut-mesa-dev freeglut3-dev mesa-common-dev
-sudo apt-get -y install lib glut-mesa-dev freeglut
-sudo apt-get -y install python-3-pip
-sudo apt-get -y install tkint
+sudo apt-get -y install libglfw3 libglfw3-dev
+sudo apt-get -y install mesa-utils libglu1-mesa-dev mesa-common-dev
+sudo apt-get -y install freeglut3 freeglut3-dev
+sudo apt-get -y install libglew-dev libglm-dev
+sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-tk
 
 # Python packages needed
 sudo python3 -m pip install tensorflow
+
